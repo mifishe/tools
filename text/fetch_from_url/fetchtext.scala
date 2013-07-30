86b543002309f1e666b89e8b754a4081d3eec3a9
mifishe <mifishe@gmail.com>
2013-07-30 14:26:08 +0300

    parsing group list from template

--- text/fetch_from_url/fetchtext.scala
+++ text/fetch_from_url/fetchtext.scala
@@ -8,9 +8,12 @@ object FetchText {
     def parse(s : String, r: Regex, g: List[Int]) =
         (r findAllIn s).matchData map (m => (g map (g1 => (g1.toString -> m.subgroups(g1)))) toMap)
 
-    def fetchText(u : String, e : String, r : Regex, g : List[Int], t: String) =
-        parse(io.Source.fromURL(u, e).mkString, r, g).foldLeft("") ((s, m) => s + applyTemplate (t, m))
+    def getGroups(t: String) =
+        (("""\{(\d*?)\}""".r findAllIn t).matchData.toList map (s => s.subgroups(0).toInt))
 
+    def fetchText(u : String, e : String, r : Regex, t: String) =
+        parse(io.Source.fromURL(u, e).mkString, r, getGroups(t)).foldLeft("") ((s, m) => s + applyTemplate (t, m))
+    
     def main(args: Array[String]) {
         val regex = "(?ism)\"item_name\">.*?<h2>(.*?)</h2>.*?item_right\">(.*?)</div.*?item_right\">(.*?)</div".r
         val template = "{0}{1}{2}\n"
@@ -19,8 +22,10 @@ object FetchText {
         val subUrls = List("email", "url", "addresses", "datetime", "markup", "other", "strings", "phones", "numbers")
         val url = "http://regexp.com.ua/category/"
         val enc = "UTF-8"
+
+        println ("taken from http://regexp.com.ua")
         
-        subUrls foreach (s => println(fetchText(url + s, enc, regex, groups, template)))
+        subUrls foreach (s => println(fetchText(url + s, enc, regex, template)))
         
     }
 
